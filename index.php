338904a1a15efec6409d9ced03c8f320468d249a
markmcpG <mark.mcp.2011@gmail.com>
2021-02-24 13:10:27 -0400

    Haciendo Buscador de pdf

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
     <link rel="stylesheet" href="vista/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="css/style.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Buscador de Certificados de Pacientes Asegurador</title>   
+    <title>Buscador de Certificados de Pacientes Asegurador</title>
+
+    <!-- PDF Objetct --> 
+    <!--<script src="vistas/plugins/pdf_object/pdfobject.js"></script>   -->
   </head>
   <body>
     <div class="content">
@@ -41,12 +44,68 @@
               <label for="">Todos los Certificados</label>
           </div>
           <div class="card-body" id="buscarPDFS">
-              <?php
-              $ruta = "temp/455226BSAID/";
-                obtener_estructura_directorios($ruta);
-              ?>
+
           </div>
       </div>
+    </div>
+
+
+    <!--=====================================
+VENTANA MODAL PARA MOSTRAR REPORTE PDF
+======================================-->
+
+    <div id="ver-pdf" class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="fichaPDF" aria-hidden="true">
+      
+      <div class="modal-dialog modal-lg">
+
+        <div class="modal-content">
+
+            <!--=====================================
+            CABEZA DEL MODAL
+            ======================================-->
+
+            <div class="modal-header bg-gradient-info">
+
+              <h5 class="modal-title" id="fichaPDF">Formulario de Impresiones</h5>
+            
+              <button type="button" class="close btnCerrarReporte" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+
+            </div>
+
+            <!--=====================================
+            CUERPO DEL MODAL
+            ======================================-->
+
+            <div class="modal-body">
+              
+              <div id="view_pdf">
+          
+
+              </div>
+
+            </div>
+
+            <!--=====================================
+            PIE DEL MODAL
+            ======================================-->
+
+            <div class="modal-footer">
+
+              <button type="button" class="btn btn-default float-left btnCerrarReporte" data-dismiss="modal">
+
+                <i class="fas fa-times"></i>
+                Cerrar
+
+              </button>
+
+            </div>
+
+        </div>
+
+      </div>
+
     </div>
 
 
--- js/funciones.js
+++ js/funciones.js
@@ -19,6 +19,7 @@ $("#btnBuscar").click(function(){
             contentType: false,
             processData: false,
             success: function(respuesta) {
+                console.log(respuesta);
                 $("#buscadorCard").show();
                 $("#buscarPDFS").html(respuesta);              			
                 swal.close();
@@ -36,5 +37,70 @@ $("#btnReset").click(function(){
     $("#buscadorCard").hide();
     $("#buscarPDFS").html("");
     location.reload();
+
+});
+
+
+$(document).on("click", "button.btn", function() {
+    alert("entro aqui");
+
+    $('#ver-pdf').modal({
+        show:true,
+        backdrop:'static'
+
+    });
+    
+
+    PDFObject.embed("temp/certificado-alta-2.pdf", "#view_pdf");
+	
+	/* var idFicha = $(this).attr("idFicha");
+	var code = $(this).data("code");
+
+	var datos = new FormData();
+
+	datos.append("fichaEpidemiologicaPDF", "fichaEpidemiologicaPDF");
+	datos.append("idFicha", idFicha);
+
+	swal.fire({
+        text: 'Procesando...',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        onOpen: () => {
+            swal.showLoading()
+        }
+    });
+
+	$.ajax({
+
+		url: "ajax/fichas.ajax.php",
+		type: "POST",
+		data: datos,
+		cache: false,
+		contentType: false,
+		processData: false,
+		//dataType: "string",
+		success: function(respuesta) {
+
+			//Para cerrar la alerta personalizada de loading
+			//console.log("esta es la respuestaMark: " + (respuesta));
+			//var aux = respuesta.split("<br />")[0];
+			//var aux = auxliar(respuesta);
+			//alert(aux);
+			swal.close();
+
+			$('#ver-pdf').modal({
+				show:true,
+				backdrop:'static'
+
+			});
+			console.log(respuesta);
+			
+
+			PDFObject.embed("temp/"+code+"/ficha-epidemiologica-"+idFicha+".pdf", "#view_pdf");
+
+		}
+
+	}); */
 
 });
